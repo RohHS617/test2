0dd0fd2da66e235cd41758f66d8b7c5cb1ab0c41
RohHS617 <wagnaria@korea.ac.kr>
2020-10-08 16:45:20 +0900

    r

---
+++ TestAdd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 2;
+    int b = 3;
+    int c = a+b;
+    cin >> a;
+    cout << a << " + " << b<< " = " << c <<endl;
+    return 0;
+}
